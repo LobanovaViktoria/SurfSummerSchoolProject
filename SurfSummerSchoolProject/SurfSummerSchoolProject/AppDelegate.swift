49b2857e3de4473b7c69ce2f026011178aa5e5ad
Viktoria Lobanova <va_klevtsova@mail.ru>
2022-08-03 16:16:40 +0300

    Project init

---
+++ SurfSummerSchoolProject/SurfSummerSchoolProject/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SurfSummerSchoolProject
+//
+//  Created by Viktoria Lobanova on 03.08.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
